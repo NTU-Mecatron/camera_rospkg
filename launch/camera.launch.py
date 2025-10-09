57db1522cd893dbd093a1e99638c690b4651db50
wei ming <alvinwm0523@gmail.com>
2025-10-09 11:09:53 +0800

    launch folder

---
+++ launch/camera.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg = get_package_share_directory('camera_driver')
+    calib = os.path.join(pkg, 'config', 'calibration.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='camera_driver',
+            executable='camera_main',
+            name='camera_publisher',
+            output='screen',
+            parameters=[{
+                'device': '/dev/video4',
+                'width': 1280,
+                'height': 720,
+                'fps': 30.0,
+                'frame_id': 'camera_optical_frame',
+                'rectify': True,
+                'camera_name': 'camera',
+                'calibration_url': f'file://{calib}'
+            }],
+        )
+    ])
